47b88bdee0517c94fbc77275b600dd4a2f5719dd
Amiya <mc.amiya@qq.com>
2023-08-20 21:23:55 +0800

    version:2023/8/20

---
+++ README.md
@@ -0,0 +1,53 @@
+<h2 align="center" style="font-weight: 600">convertM3U8ToMP3</h2>
+<p align="center">
+    <s>更方便</s>的转换m3u8电台到mp3流
+    <br />
+    Version: 2023/8/20
+    <!-- <a href="https://music.qier222.com" target="blank"><strong>🌎 访问DEMO</strong></a>  |  
+    <a href="#%EF%B8%8F-安装" target="blank"><strong>📦️ 下载安装包</strong></a>  |  
+    <a href="https://t.me/yesplaymusic" target="blank"><strong>💬 加入交流群</strong></a>
+    <br />
+    <br /> -->
+  </p>
+</p>
+
+## ✨ 特性
+
+- ✅ 使用 Go 开发 性能至上
+- 📃 支持主流的m3u8电台格式转换mp3流
+- 🧩 感谢万能的ffmpeg
+- 💾 修改后下载可自行修改config.json配置内容
+- ✔️ 可以支持所有m3u8音频转换
+- 📻 支持在欧卡2中使用
+- 🖥️ 无UI界面 无感转换
+
+## 📦️ 打包
+
+- 🛠 直接 git clone 然后安装依赖打包即可
+
+```shell
+# 打包
+go build
+```
+
+## 💻 运行
+
+下载已构建的版本，双击运行，首次运行只会生成配置文件，再次运行即可使用。
+
+
+## ☑️ Todo
+
+1. 添加到托盘，可以自行选择是否退出程序（不必使用任务管理器）
+2. 可以在软件中设置开机自启不必去`shell:Common Startup`
+
+欢迎提 Issue 和 Pull request。
+
+## 📜 开源许可
+
+本项目仅供个人学习研究使用，禁止用于商业及非法用途。
+
+基于 [GNU GPL v3](https://www.gnu.org/licenses/gpl-3.0.en.html#license-text) 许可进行开源。
+
+## 🖼️ 截图
+
+无Ui界面 无痛转换（确信）
---
+++ main.go
@@ -0,0 +1,135 @@
+//go:generate goversioninfo
+package main
+
+import (
+	_ "embed"
+	"encoding/json"
+	"github.com/fufuok/favicon"
+	"github.com/gin-gonic/gin"
+	"io"
+	"log"
+	"net/http"
+	"os"
+	"os/exec"
+	"syscall"
+	"time"
+)
+
+type Config struct {
+	Streams    map[string]string `json:"Streams"`
+	IpPort     string            `json:"ipPort"`
+	FfmpegPath string            `json:"ffmpegPath"`
+}
+
+//go:embed favicon.ico
+var favData []byte
+
+func main() {
+	// 非调试模式
+	gin.SetMode(gin.ReleaseMode)
+
+	config, _ := loadConfig("./config.json")
+
+	r := gin.Default()
+	//favicon.ico
+
+	//部分场景失效 比如mp3Stream
+	//r.StaticFile("/favicon.ico", "./favicon.ico")
+	//打包后仍需本地有ico文件 不然会报错 thinkerou
+	//r.Use(favicon.New("./favicon.ico"))
+	//使用嵌入 把图标嵌入到打包的可执行文件 fufuok
+	r.Use(favicon.New(favicon.Config{
+		FileData: favData,
+	}))
+
+	r.GET("/", func(context *gin.Context) {
+		context.String(http.StatusOK, "Server Run Successful!\nVersion:2023/8/20")
+	})
+
+	r.GET("/:streamID", func(c *gin.Context) {
+		config, err := loadConfig("./config.json")
+		if err != nil {
+			c.String(http.StatusInternalServerError, "Error loading config")
+			return
+		}
+
+		streamID := c.Param("streamID")
+		m3u8URL, found := config.Streams[streamID]
+		if !found {
+			c.String(http.StatusNotFound, "Stream not found")
+			return
+		}
+
+		mp3Stream, ffmpegCmd, err := convertM3U8ToMP3(config.FfmpegPath, m3u8URL)
+		if err != nil {
+			c.String(http.StatusInternalServerError, "Error converting M3U8 to MP3")
+			return
+		}
+		defer func() {
+			// 等待一段时间，以确保进程有足够的时间完成任务并退出
+			time.Sleep(5 * time.Second) // 等待时间
+			ffmpegCmd.Process.Kill()
+		}()
+
+		c.Header("Content-Type", "audio/mpeg")
+		c.Status(http.StatusOK)
+		io.Copy(c.Writer, mp3Stream)
+	})
+
+	log.Printf("[main] %s", "Main函数运行中")
+	ipAddr := "localhost:" + config.IpPort
+	log.Printf("[main] IpAddress:http://%s/\n", ipAddr)
+
+	r.Run(":" + config.IpPort)
+}
+
+func loadConfig(filename string) (*Config, error) {
+	log.Printf("[loadConfig] Loading config from:'%s'", filename)
+	file, err := os.Open(filename)
+	//file, err := os.OpenFile(filename, os.O_CREATE, 0)
+	if err != nil {
+		os.Create(filename)
+		content := `{
+  "ipPort": "24748",
+  "ffmpegPath": "./ffmpeg",
+  "Streams": {
+    "广东羊城交通台": "http://ls.qingting.fm/live/1262/64k.m3u8?format=aac",
+    "广东广播电视台股市广播": "http://ls.qingting.fm/live/4847/64k.m3u8?format=aac",
+    "广东珠江经济电台": "http://ls.qingting.fm/live/1259/64k.m3u8?format=aac",
+    "广东广播电视台文体广播": "http://ls.qingting.fm/live/471/64k.m3u8?format=aac",
+    "广东音乐之声": "http://ls.qingting.fm/live/1260/64k.m3u8?format=aac",
+    "佛山电台FM906": "http://ls.qingting.fm/live/1264/64k.m3u8?format=aac",
+    "番禺电台畅快1017": "http://ls.qingting.fm/live/20212427/64k.m3u8?format=aac",
+    "广州MYFM 88.0": "http://ls.qingting.fm/live/20194/64k.m3u8?format=aac"
+  }
+}`
+		os.WriteFile(filename, []byte(content), 0666)
+		return nil, err
+	}
+	defer file.Close()
+
+	config := &Config{}
+	decoder := json.NewDecoder(file)
+	err = decoder.Decode(config)
+	if err != nil {
+		return nil, err
+	}
+	return config, nil
+}
+
+func convertM3U8ToMP3(ffmpegPath string, m3u8URL string) (io.Reader, *exec.Cmd, error) {
+	cmd := exec.Command(ffmpegPath, "-i", m3u8URL, "-f", "mp3", "-")
+	cmd.SysProcAttr = &syscall.SysProcAttr{HideWindow: true}
+	cmd.Stderr = os.Stderr
+	mp3Stream, err := cmd.StdoutPipe()
+	if err != nil {
+		return nil, nil, err
+	}
+
+	err = cmd.Start()
+	if err != nil {
+		return nil, nil, err
+	}
+
+	return mp3Stream, cmd, nil
+}

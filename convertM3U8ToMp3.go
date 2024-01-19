955614fce9f3a83f0f2e134563e50bdf23d6820b
mcAmiya <mc.amiya@qq.com>
2024-01-19 22:17:46 +0800

    fix: 修复非windows环境下隐藏窗口方法的差异导致的bug

---
+++ convertM3U8ToMp3.go
@@ -0,0 +1,26 @@
+//go:build !windows
+
+package main
+
+import (
+	"io"
+	"os"
+	"os/exec"
+)
+
+func convertM3U8ToMP3(ffmpegPath string, m3u8URL string) (io.Reader, *exec.Cmd, error) {
+	cmd := exec.Command(ffmpegPath, "-i", m3u8URL, "-f", "mp3", "-")
+
+	cmd.Stderr = os.Stderr
+	mp3Stream, err := cmd.StdoutPipe()
+	if err != nil {
+		return nil, nil, err
+	}
+
+	err = cmd.Start()
+	if err != nil {
+		return nil, nil, err
+	}
+
+	return mp3Stream, cmd, nil
+}
--- main.go
+++ main.go
@@ -132,20 +132,3 @@ func loadConfig(filename string) (*Config, error) {
 	}
 	return config, nil
 }
-
-func convertM3U8ToMP3(ffmpegPath string, m3u8URL string) (io.Reader, *exec.Cmd, error) {
-	cmd := exec.Command(ffmpegPath, "-i", m3u8URL, "-f", "mp3", "-")
-	cmd.SysProcAttr = &syscall.SysProcAttr{HideWindow: true}
-	cmd.Stderr = os.Stderr
-	mp3Stream, err := cmd.StdoutPipe()
-	if err != nil {
-		return nil, nil, err
-	}
-
-	err = cmd.Start()
-	if err != nil {
-		return nil, nil, err
-	}
-
-	return mp3Stream, cmd, nil
-}
